ac753808d6b35bb64f76fba4f183a0dd139ce7de
divyanshukumar305-hub <divyanshukumar305@gmail.com>
2026-08-11 22:51:39 +0530

    my new code push

---
+++ perents/main.tf
@@ -0,0 +1,31 @@
+module "resource_group" {
+  source = "../child/resource_group"
+  rgs    = var.rgs
+
+}
+module "virtual_network" {
+  depends_on = [module.resource_group]
+  source     = "../child/vnet"
+  vnets      = var.vnets
+
+}
+
+module "subnet_network" {
+  depends_on = [module.virtual_network,module.resource_group]
+  source     = "../child/subnet"
+  subnets    = var.subnets
+
+
+}
+module "public_ip" {
+  depends_on = [module.resource_group]
+  source     = "../child/pip"
+  pips       = var.pips
+}
+
+module "virtual_machine" {
+  depends_on = [module.resource_group, module.public_ip]
+  source     = "../child/virtual_machine"
+  vms        = var.vms
+
+}
